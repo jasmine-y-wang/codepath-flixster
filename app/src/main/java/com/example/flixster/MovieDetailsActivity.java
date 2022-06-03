57b805cd1689c22fcf26cdf8f2d5535f86c0b42c
jasmine-y-wang <jasminewang@fb.com>
2022-06-03 14:21:38 -0700

    youtube on details page, gitignore with secrets.xml

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.databinding.ActivityMovieDetailsBinding;
 import com.example.flixster.models.Movie;
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -14,11 +16,16 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import okhttp3.Headers;
 
 public class MovieDetailsActivity extends YouTubeBaseActivity {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyA5GHARyE7L8_9AkOfYeJ6dfb-3TEq3bdE";
+    public static final String TAG = "MovieDetailsActivity";
 
     // movie to display
     Movie movie;
@@ -41,7 +48,7 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
-        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+        Log.d(TAG, String.format("Showing details for '%s'", movie.getTitle()));
 
         // set title and overview
         tvTitle.setText(movie.getTitle());
@@ -51,18 +58,40 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
 
-
         // get video from API
+        AsyncHttpClient client = new AsyncHttpClient();
+        String movie_url = String.format("https://api.themoviedb.org/3/movie/%d/videos?api_key=%s",
+                movie.getId(), getString(R.string.movie_api_key));
+        client.get(movie_url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    String videoId = results.getJSONObject(0).getString("key");
+                    Log.i(TAG, "Video ID: " + videoId);
+                    initializeYoutube(videoId);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                    e.printStackTrace();
+                }
+            }
 
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure: " + response);
+            }
+        });
+    }
 
-        // temporary test video id
-        final String videoId = "Y9dr2zw-TXQ";
-
+    private void initializeYoutube(String videoId) {
         // resolve player view from the layout
         YouTubePlayerView playerView = (YouTubePlayerView) binding.player;
 
         // initialize with API key
-        playerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+        playerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 // do any work here to cue video, play video, etc
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -55,6 +55,10 @@ public class Movie {
         return overview;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
     public Double getVoteAverage() {
         return voteAverage;
     }
